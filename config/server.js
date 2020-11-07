bc94702e4560763ed78f282200dc834626ee3466
Entreprenerd <valeranijr@gmail.com>
2020-11-07 16:32:05 +0100

    3.2.5 with Products

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8c138357c06a40b6f15cd4fcd87e2606'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '54fa3936-3e57-4b91-83cd-debc9ed99419'
+};
