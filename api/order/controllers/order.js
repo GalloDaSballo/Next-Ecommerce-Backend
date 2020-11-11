cec3907cea989bedbab92ef64b97e6b7146837dd
Entreprenerd <valeranijr@gmail.com>
2020-11-11 15:00:51 +0100

    Get the Stripe Stuff

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,6 +1,11 @@
 'use strict';
 const { sanitizeEntity } = require('strapi-utils');
 
+const stripe = require('stripe')(process.env.STRIPE_PK)
+
+const fromDecimalToInt = (number) => parseInt(number * 100)
+
+
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
@@ -32,4 +37,53 @@ module.exports = {
         const entity = await strapi.services.order.findOne({ id, user: user.id });
         return sanitizeEntity(entity, { model: strapi.models.order });
     },
+
+
+    async create(ctx) {
+        const BASE_URL = ctx.request.headers.origin || 'http://localhost:3000' //So we can redirect back
+    
+        const { product } = ctx.request.body
+        if(!product){
+            return res.status(400).send({error: "Please add a product to body"})
+        }
+
+        //Retrieve the real product here
+        const realProduct = await strapi.services.product.findOne({ id: product.id })
+        if(!realProduct){
+            return res.status(404).send({error: "This product doesn't exist"})
+        }
+
+        const {user} = ctx.state //From Magic Plugin
+
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            line_items: [
+                {
+                    price_data: {
+                        currency: "usd",
+                        product_data: {
+                            name: realProduct.name
+                        },
+                        unit_amount: fromDecimalToInt(realProduct.price),
+                    },
+                    quantity: 1,
+                },
+            ],
+            customer_email: user.email, //Automatically added by Magic Link
+            mode: "payment",
+            success_url: `${BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url: BASE_URL,
+        })
+        
+        //TODO Create Temp Order here
+        const newOrder = await strapi.services.order.create({
+            user: user.id,
+            product: realProduct.id,
+            total: realProduct.price,
+            status: 'unpaid',
+            checkout_session: session.id
+        })
+
+        return { id: session.id }
+    }
 };
